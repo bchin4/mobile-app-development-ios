c28d083017b00e38a18174d435713299c801086d
Bobby St. Jacques, Jr <mobappdevguy@gmail.com>
2016-09-07 23:09:58 -0400

    Commented the model.

--- Lectures/Lecture03/HelloPlanets/HelloPlanets/Planets.swift
+++ Lectures/Lecture03/HelloPlanets/HelloPlanets/Planets.swift
@@ -8,7 +8,12 @@
 
 import Foundation
 
+//
+// This class is the model (state and core application logic) for the
+// "Hello, Planets!" app.
+//
 class Planets {
+    // the  available planets, in order
     private let planets = ["Mercury",
                            "Venus",
                            "Earth",
@@ -18,8 +23,12 @@ class Planets {
                            "Uranus",
                            "Neptune"]
     
-    private var planetIndex = -1
+    // the index of the current planet (set up to be Earth when the
+    // app first loads
+    private var planetIndex = 1
     
+    // simple method to return the next planet.  If it gets to the
+    // end of the array, it starts over
     func getNextPlanet() -> String {
         planetIndex = planetIndex + 1
         if planetIndex == planets.count {
@@ -28,6 +37,8 @@ class Planets {
         return planets[planetIndex]
     }
     
+    // simple method to return the appropriate "Hello" string based
+    // on the current planet.
     func hello() -> String {
         return "Hello, " + planets[planetIndex] + "!"
     }
